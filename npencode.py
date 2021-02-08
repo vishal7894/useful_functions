cf3bb6f0b64fcda80ec58125d8b99b7b614017aa
Vishal <47276786+vishal7894@users.noreply.github.com>
2021-02-08 12:28:47 +0530

    Update npencode.py

--- npencode.py
+++ npencode.py
@@ -1,6 +1,13 @@
 import numpy as np
 import json
 
+# copy this code block and paste in 
+# the file where we get the error in json.dumps
+# where int64 is causing issues
+
+
+# while using json.dumps(<json_obj>, indent=<4>, cls=NpEncoder)
+
 class NpEncoder(json.JSONEncoder):
 
     """
