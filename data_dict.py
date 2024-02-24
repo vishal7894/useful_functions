893e4569636d5fb9b67eeec03b1c23ac290938a3
Vishal <47276786+vishal7894@users.noreply.github.com>
2024-02-24 17:21:39 +0530

    Update data_dict.py

--- data_dict.py
+++ data_dict.py
@@ -1,14 +1,15 @@
 def get_data_dictionary(data):
     data_dict = pd.DataFrame(columns = ['Column','Count','Unique Values','Range',
                                         'Null values','Possible Values'])
-    for i in data.columns:
-        col = i
+    for col in data.columns:
         count = data[col].shape[0]
         unique_values = data[col].nunique()
-        range = f"{data[col].min()} - {data[col].max()}"
+        if unique_values>1:
+            range = f"{data[col].min()} - {data[col].max()}"
+        else:
+            range = np.nan
         nulls = data[col].isna().sum()
         values = list(data[col].sample(25))
         values = list(set(values))[:5]
-        print(unique_values)
         data_dict.loc[len(data_dict)] = [col,count,unique_values,range,nulls,values]
     return data_dict
